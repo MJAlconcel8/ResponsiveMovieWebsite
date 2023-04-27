f78fc036559944c19ee3e1539981e08e003bb2c2
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2023-04-27 17:43:41 -0400

    js file updated
    added the api request from TMDB api

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+const APIKEY = 'api_key=aadab7b55997e452fe229067e700adf9';
+const BASEURL = 'https://api.themoviedb.org/3/';
+const APIURL = BASEURL + '/discover/movie?sort_by-popularity.desc'+APIKEY;
